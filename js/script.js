6036b81d7e40a98fc76415e7c9c7530034677bcb
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-06 19:59:45 +0630

    Removed classList when close the navigation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-const hamburger = document.querySelector('.humburger');
-const navmenu = document.querySelector('.nav-list');
-const body = document.querySelector('.content-section');
+const hamburger = document.querySelector(".humburger");
+const navmenu = document.querySelector(".nav-list");
+const body = document.querySelector(".content-section");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navmenu.classList.toggle('active');
-  body.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+  body.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navmenu.classList.remove("active");
+    body.classList.remove("active");
+  })
+);
