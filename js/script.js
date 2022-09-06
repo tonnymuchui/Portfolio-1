b8284d752c0643a763f86b88ab5349b0e4161c8a
tonnymuchui <tonykanyingah@gmail.com>
2022-09-06 15:45:12 +0300

    fix linters errors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const hamburger = document.querySelector(".humburger");
-const navmenu = document.querySelector(".nav-list");
+const hamburger = document.querySelector('.humburger');
+const navmenu = document.querySelector('.nav-list');
 const body = document.querySelector('.content-section');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navmenu.classList.toggle('active');
   body.classList.toggle('active');
-})
+});
